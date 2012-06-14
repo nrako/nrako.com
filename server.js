ab8c9ed7e48578908dc64c6bf1d417363977dfdd
Nicolas Rakoto <nico@nrako.com>
2012-06-14 03:47:33 +0200

    test fleet

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mvc.boot(app);
 // run socket.io
 //io.run(server, mvc.sessionStore);
 
-//console.dir(process.env);
+console.dir(process.env);
 
 server.listen(port);
 console.log("Express server listening on port %d in %s mode for host %s", server.address().port, server.settings.env, config.hostname);
