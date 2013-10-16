c4022412a0a259be8365830f445a5f5498b7cd6b
Nicolas Rakoto <nico@nrako.com>
2013-10-16 18:29:21 +0200

    cleanup

--- forms.js
+++
@@ -1,8 +0,0 @@
-var form = require('express-form'),
-    field = form.field;
-
-exports.contact = form(
-  field('headline').trim().required(),
-  field('msg').trim().required(),
-  field('email').trim().isEmail().required()
-);
