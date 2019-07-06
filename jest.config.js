ee6bcedfd8942eb6807d067157f7a1c92fc33d6d
Nicholas Rakoto <nico@nrako.com>
2019-07-06 15:20:03 +0200

    chores, upgrade most packages, yolo

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'jest-puppeteer',
   setupTestFrameworkScriptFile: './__tests__/.support/setupTestFramework.js',
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/__tests__/.support'],
+  testRegex: './*\\.spec\\.js$',
 }
